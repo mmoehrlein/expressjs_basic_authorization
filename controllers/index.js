6cb756050e938d612a0a3745c49b8e7313fda4d5
Michael Möhrlein <github@mmoehrlein.de>
2017-11-14 11:28:25 +0100

    starting to implement dormip

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+
 router.use('/users', require('./usersController'));
+router.use('/dormip', require('./dormIpController'));
 
 router.get('/', function(req, res){
     res.render('home', {
